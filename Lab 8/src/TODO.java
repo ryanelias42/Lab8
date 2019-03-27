4379955bc138f981202969b70e632ee1884e5cd9
Ryan E. <elias.ryan.matthew@gmail.com>
2019-03-26 20:08:20 -0500

    added TODO

---
+++ Lab 8/src/TODO.java
@@ -0,0 +1,35 @@
+
+public class TODO {
+	/*
+		Lab Section_________: 11
+		Last Modified_______: 2019-03-26
+		OU ID_______________: 113481704
+		OU EMAIL____________: ryanelias@ou.edu
+
+		Tasks:
+		1. Complete Enums color and shape
+		2. Complete GamePieceAppearance
+		3. complete GamePiece Enum
+		4. complete complete Location Enum
+		5. complete BoardGame class
+		6. Testing
+
+		Estimated Time (minutes):
+		1. 15
+		2. 45
+		3. 30
+		4. 2
+		5. 60
+		6. 75
+
+		Actual Time (minutes):
+		1. 15
+		2. 45
+		3. 45
+		4. 2
+		5. 70
+		6. 90
+		*/
+
+
+}
